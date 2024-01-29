590d0a9ffbbf0138b225ffca9c13ca3d6dcc7ab7
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-01-29 17:23:09 +0200

    Update HexGrid.cs

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class HexGrid : MonoBehaviour
 {
-    // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Debug.Log("Hello, World!");
     }
 }
