ce6804c8ce50af64978548a39bab63b9a4f45f0d
Aleksi <79052109+Haxeli@users.noreply.github.com>
2024-01-31 19:43:50 +0200

    Added mouse input

--- Assets/Scripts/MouseController.cs
+++ Assets/Scripts/MouseController.cs
@@ -9,5 +9,21 @@ public class MouseController : Singleton<MouseController>
     public Action<RaycastHit> OnRightMouseClick;
     public Action<RaycastHit> OnMiddleMouseClick;
 
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckMouseClick(0);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            CheckMouseClick(1);
+        }
+        if (Input.GetMouseButtonDown(2))
+        {
+            CheckMouseClick(2);
+        }
+    }
 
+    
 }
